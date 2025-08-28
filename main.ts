0286cc2202d9d4baf2afc044d9aebfb0507bcd2e
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 06:28:12 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+let tiempoa = 0
+let tiempob = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    tiempoa = input.runningTime() / 1000
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    tiempob = input.runningTime() / 1000
+    basic.showNumber(tiempob - tiempoa)
+})
